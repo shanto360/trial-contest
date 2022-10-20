9faac3a05e93cd37fa48dd002f88151d541b20e1
shanto360 <shanto.360@yahoo.com>
2022-10-21 01:40:52 +0600

    welcome

---
+++ Welcome/Welcome.c
@@ -0,0 +1,8 @@
+// Problem Statement
+// Welcome to the Phitron trial programming contest. This is just a simple problem to welcome you. You just need to say Hello! to pass this problem. As simple as that.
+#include<stdio.h>
+int main()
+{
+    printf("Hello!");
+    return 0;
+}
